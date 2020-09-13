c2eb8cd71e1ef94a8a8a78d617acf4e57d197f2c
Nosayba Ahmed <Elmahnu45@gmail.com>
2020-09-13 10:51:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,27 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+ rootElem.textContent =`Got ${episodeList.length} episode(s)`
+  for (let i=0; i < episodeList.length;i++){
+  let divElement = document.createElement("div");
+ divElement.style.backgroundColor = "green";
+  let headName = document.createElement("h4");
+  headName.textContent= episodeList[i].name + episodeList[i].season + episodeList[i].number ;
+  divElement.appendChild(headName);
+
+  let imageTag = document.createElement("img");
+  imageTag.src= episodeList[i].image.medium ;
+  divElement.appendChild(imageTag);
+
+  let textParagraph = document.createElement("p");
+    textParagraph.innerHTML =episodeList[i].summary;
+    divElement.appendChild(textParagraph);
+
+    rootElem.appendChild(divElement);
+
+
+
+  }
 }
 
 window.onload = setup;
